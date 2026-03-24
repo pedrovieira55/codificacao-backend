03e15d88b968932a326388c0ee07077f1221253e
pedrovieira55 <pedrohenrique.13carlos@gmail.com>
2026-03-24 16:42:44 -0300

    Atividade24-03-26-Edio
    envio atividade Edio

--- 24-03-2026-Edio/src/services/jogoService.js
+++ 24-03-2026-Edio/src/services/jogoService.js
@@ -4,7 +4,7 @@ const jogos = [
   { id: 3, nome: "Assassin's Creed: Unity",  preco: 150.00 },
   { id: 4, nome: "Naruto Storm 3",  preco: 70.00 }
 ];
-// 1. Mudamos a primeira letra para Maiúscula para não dar conflito
+
 class JogoService { 
   getAll() {
     return jogos;
@@ -15,5 +15,5 @@ class JogoService {
   }
 }
 
-// 2. Usamos o export no formato CommonJS, igual você fez no arquivo de rotas
+
 export const jogoService = new JogoService();
